e7afa1321319abea52da6bdeaba08ff3ecaf2c4d
s.kumar.bhalotia <s.kumar.bhalotia@accenture.com>
2015-05-11 16:28:57 +0530

    jsp servlet - first commit

---
+++ ServletCookieExample/src/com/journaldev/servlet/session/LoginServlet.java
@@ -0,0 +1,71 @@
+package com.journaldev.servlet.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private final String userID = "Pankaj";
+	private final String password = "journaldev";
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getParameter("user");
+		String pwd = request.getParameter("pwd");
+
+		if (userID.equals(user) && password.equals(pwd)) {
+			HttpSession session = request.getSession();
+			session.setAttribute("user", "Pankaj");
+			// setting session to expiry in 30 mins
+			session.setMaxInactiveInterval(30 * 60);
+			
+			Cookie loginCookie = new Cookie("user", user);
+			loginCookie.setMaxAge(30 * 60);
+			response.addCookie(loginCookie);
+			String encodedURL = response.encodeRedirectURL("loginsuccess.jsp");
+			response.sendRedirect(encodedURL);
+		//	response.sendRedirect("loginsuccess.jsp");
+		} else {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher(
+					"/login.html");
+			PrintWriter out = response.getWriter();
+			out.println("<font color=red>Either user name or password is wrong.</font>");
+			rd.include(request, response);
+		}
+	}
+
+}
